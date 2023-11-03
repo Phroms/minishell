9c62dbf30ae0481b1d41d04493fed9cadb872dc4
Phroms <hernangrim.10@gmail.com>
2023-11-03 13:40:17 +0100

    echo casi terminadoo

--- include/Libft/libft.h
+++ include/Libft/libft.h
@@ -58,7 +58,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
-	/*	ADICIONALES	*/
+	/* ADICIONALES */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
@@ -71,11 +71,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-	/*	CREADOS PARA EL LIBFT	*/
-
-char	*ft_append(char **dst, char *src);
-void	*ft_free(void **pointer);
 
 	/*	BONUSSSSSSS LIBFT	*/
 

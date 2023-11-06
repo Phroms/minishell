599773885d136c0855a781088d95e9b0622aa521
Phroms <hernangrim.10@gmail.com>
2023-11-06 20:07:17 +0100

    singal falla

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: agrimald <agrimald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 18:53:56 by agrimald          #+#    #+#             */
-/*   Updated: 2023/10/31 19:11:09 by agrimald         ###   ########.fr       */
+/*   Updated: 2023/11/06 17:46:55 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
+ #include <signal.h>
  #include <ctype.h>
+ #include <limits.h>
+ #include <unstd.h>
 
 
 typedef struct s_env
@@ -33,5 +36,9 @@ void	duplicate_env(char **env_cpy, char *or_env);
 char	*cpy_or_env(char *or_env);
 char	*ft_getenv(char *variable, char **env);
 int		main(int argc, char **argv, char **envp);
+
+	/*		PWD		*/
+
+void	pwd(void);
 
 #endif
--- src/commands/pwd.c
+++ src/commands/pwd.c
@@ -6,13 +6,33 @@
 /*   By: agrimald <agrimald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 13:20:28 by agrimald          #+#    #+#             */
-/*   Updated: 2023/11/03 13:34:29 by agrimald         ###   ########.fr       */
+/*   Updated: 2023/11/06 17:28:10 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.c"
+//#include "../include/minishell.c"
 
-int	pwd(void)
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	pwd(void)
 {
-	
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (pwd != NULL)
+	{
+		printf("%s", pwd);
+		free(pwd);
+	}
 }
+
+/*int main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc == 2)
+		pwd();
+	return (0);
+}*/
